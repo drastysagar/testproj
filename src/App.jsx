ff46ffc9fa962a693d9b47378d1ab320f0052128
Vivek Pramanik <vivekpramanikbundu@gmail.com>
2023-10-30 23:51:24 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ExampleNavbarTwo } from './Navbar'
+import { FeatureTwo } from './Features'
+import { FooterOne } from './Footer'
+
+const App = () => {
+  return (
+    <div>
+      <ExampleNavbarTwo />
+      <FeatureTwo />
+      <FooterOne />
+    </div>
+  )
+}
+
+export default App
